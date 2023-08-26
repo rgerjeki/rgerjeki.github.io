5706a2865837fd3a631636c49ee391482504499a
rgerjeki <reesegerjekian@icloud.com>
2023-08-26 13:58:22 -0400

    updated index/python/css, added js

---
+++ js/hidden.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Wait for 3 seconds
+    setTimeout(function() {
+      // Get the block element
+      const block = document.getElementById('fadeInBlock');
+      
+      // Remove the 'hidden' class and add the 'visible' class
+      block.classList.remove('hidden');
+      block.classList.add('visible');
+    }, 2000);
+  });
+document.addEventListener('DOMContentLoaded', function() {
+// Wait for 3 seconds
+setTimeout(function() {
+    // Get the block element
+    const block = document.getElementById('fadeInBlock2');
+    
+    // Remove the 'hidden' class and add the 'visible' class
+    block.classList.remove('hidden');
+    block.classList.add('visible');
+}, 2000);
+});
+document.addEventListener('DOMContentLoaded', function() {
+    // Wait for 3 seconds
+    setTimeout(function() {
+    // Get the block element
+    const block = document.getElementById('fadeInBlock3');
+    
+    // Remove the 'hidden' class and add the 'visible' class
+    block.classList.remove('hidden');
+    block.classList.add('visible');
+}, 2000);
+});
+document.addEventListener('DOMContentLoaded', function() {
+    // Wait for 3 seconds
+    setTimeout(function() {
+    // Get the block element
+    const block = document.getElementById('fadeInBlock4');
+    
+    // Remove the 'hidden' class and add the 'visible' class
+    block.classList.remove('hidden');
+    block.classList.add('visible');
+}, 2000);
+});
+document.addEventListener('DOMContentLoaded', function() {
+    // Wait for 3 seconds
+    setTimeout(function() {
+    // Get the block element
+    const block = document.getElementById('fadeInBlock5');
+    
+    // Remove the 'hidden' class and add the 'visible' class
+    block.classList.remove('hidden');
+    block.classList.add('visible');
+}, 2000);
+});
